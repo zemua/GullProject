967aa8179451c8bd9ed2a25f0148245b743b18cc
miguel <48907268+zemua@users.noreply.github.com>
2021-05-06 18:32:13 +0200

    disabling modification of atributo data type
    prevents inconsistencies in the datatype of the database for a given
    attribute

--- GullCampoService/src/test/java/devs/mrp/gullproject/controller/ConsultaControllerTest.java
+++ GullCampoService/src/test/java/devs/mrp/gullproject/controller/ConsultaControllerTest.java
@@ -268,15 +268,6 @@ class ConsultaControllerTest {
 					Assertions.assertThat(response.getResponseBody()).asString()
 						.contains("Todas Las Consultas")
 						.contains("Crear nueva consulta");
-					/**
-					 * Now the info is passed through rest to the javascript datasheet
-					 * so we don't include below data in the template anymore
-					 */
-						/*.contains("consulta 1")
-						.contains("estado 1")
-						.contains("revisar")
-						.contains("consulta 2")
-						.contains("estado 2");*/
 			});
 	}
 	
