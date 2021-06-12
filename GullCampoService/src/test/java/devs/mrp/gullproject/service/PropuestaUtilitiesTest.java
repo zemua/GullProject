d53092b37dbc209f79896221154c42a8b31900fd
miguel <48907268+zemua@users.noreply.github.com>
2021-06-12 16:50:51 +0200

    testing

--- GullCampoService/src/test/java/devs/mrp/gullproject/service/PropuestaUtilitiesTest.java
+++ GullCampoService/src/test/java/devs/mrp/gullproject/service/PropuestaUtilitiesTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,6 +21,8 @@ import org.springframework.validation.BindException;
 import devs.mrp.gullproject.domains.AtributoForCampo;
 import devs.mrp.gullproject.domains.Propuesta;
 import devs.mrp.gullproject.domains.PropuestaCliente;
+import devs.mrp.gullproject.domains.dto.AtributoForFormDto;
+import devs.mrp.gullproject.domains.dto.AttributesListDto;
 import devs.mrp.gullproject.domains.dto.WrapAtributosForCampoDto;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
@@ -33,12 +36,16 @@ public class PropuestaUtilitiesTest {
 
 	@MockBean
 	ConsultaService consultaService;
+	@MockBean
+	AtributoServiceProxyWebClient atributoService;
 	
 	PropuestaUtilities propuestaUtilities;
+	ModelMapper modelMapper;
 	
 	@Autowired
-	public PropuestaUtilitiesTest(PropuestaUtilities propuestaUtilities) {
+	public PropuestaUtilitiesTest(PropuestaUtilities propuestaUtilities, ModelMapper modelMapper) {
 		this.propuestaUtilities = propuestaUtilities;
+		this.modelMapper = modelMapper;
 	}
 	
 	Propuesta propuesta;
@@ -46,6 +53,7 @@ public class PropuestaUtilitiesTest {
 	AtributoForCampo att2;
 	WrapAtributosForCampoDto wrapAtributosForCampoDto;
 	List<AtributoForCampo> listAtts;
+	AtributoForCampo att3;
 	
 	@BeforeEach
 	void setup() {
@@ -63,6 +71,12 @@ public class PropuestaUtilitiesTest {
 		propuesta.addAttribute(att1);
 		propuesta.addAttribute(att2);
 		
+		att3 = new AtributoForCampo();
+		att3.setId("att3id");
+		att3.setName("nameatt3");
+		att3.setTipo("DESCRIPCION");
+		att3.setOrder(2);
+		
 		listAtts = new ArrayList<>();
 		listAtts.add(att1);
 		listAtts.add(att2);
@@ -71,6 +85,8 @@ public class PropuestaUtilitiesTest {
 		wrapAtributosForCampoDto.setAtributos(listAtts);
 		
 		when(consultaService.findAttributesByPropuestaId(ArgumentMatchers.eq(propuesta.getId()))).thenReturn(Flux.just(att1, att2));
+		when(consultaService.findPropuestaByPropuestaId(propuesta.getId())).thenReturn(Mono.just(propuesta));
+		when(atributoService.getAllAtributos()).thenReturn(Flux.just(att1, att2, att3));
 	}
 	
 	@Test
@@ -90,7 +106,6 @@ public class PropuestaUtilitiesTest {
 	void testAtributosFromWrapAndValidateBelongsToPropuesta() {
 		BindException bindingResult = new BindException(wrapAtributosForCampoDto, "wrapAtributosForCampoDto");
 		
-		log.debug("should be ok");
 		AtributoForCampo atta = new AtributoForCampo();
 		atta.setLocalIdentifier(att1.getLocalIdentifier());
 		atta.setOrder(3);
@@ -100,6 +115,8 @@ public class PropuestaUtilitiesTest {
 		List<AtributoForCampo> atts = new ArrayList<>();
 		atts.add(atta);
 		atts.add(attb);
+		
+		log.debug("should be ok");
 		wrapAtributosForCampoDto.setAtributos(atts);
 		Mono<List<AtributoForCampo>> list = propuestaUtilities.atributosOrderFromWrapAndValidateBelongsToPropuesta(wrapAtributosForCampoDto, bindingResult, propuesta.getId());
 		StepVerifier.create(list)
@@ -129,6 +146,51 @@ public class PropuestaUtilitiesTest {
 				assertEquals(att2.getName(), l.get(0).getName());
 				assertEquals(att2.getTipo(), l.get(0).getTipo());
 				assertEquals(attb.getOrder(), l.get(0).getOrder());
+			})
+			.expectComplete()
+			.verify()
+			;
+	}
+	
+	@Test
+	void testGetAttributesAndMarkActualFromProposal() {
+		Mono<AttributesListDto> listDto = propuestaUtilities.getAttributesAndMarkActualFromProposal(propuesta.getId());
+		StepVerifier.create(listDto)
+			.assertNext(lDto -> {
+				assertEquals(3, lDto.getAttributes().size());
+				assertEquals(att1.getId(), lDto.getAttributes().get(0).getId());
+				assertEquals(true, lDto.getAttributes().get(0).getSelected());
+				assertEquals(att2.getId(), lDto.getAttributes().get(1).getId());
+				assertEquals(true, lDto.getAttributes().get(1).getSelected());
+				assertEquals(att3.getId(), lDto.getAttributes().get(2).getId());
+				assertEquals(false, lDto.getAttributes().get(2).getSelected());
+			})
+			.expectComplete()
+			.verify()
+			;
+	}
+	
+	@Test
+	void testListOfSelectedAttributes() {
+		AtributoForFormDto dto1 = modelMapper.map(att1, AtributoForFormDto.class);
+		dto1.setSelected(true);
+		AtributoForFormDto dto2 = modelMapper.map(att2, AtributoForFormDto.class);
+		dto2.setSelected(true);
+		AtributoForFormDto dto3 = modelMapper.map(att3, AtributoForFormDto.class);
+		dto3.setSelected(false);
+		List<AtributoForFormDto> dtos = new ArrayList<>();
+		dtos.add(dto1);
+		dtos.add(dto2);
+		dtos.add(dto3);
+		AttributesListDto atts = new AttributesListDto(dtos);
+		
+		Flux<AtributoForCampo> fl = propuestaUtilities.listOfSelectedAttributes(atts);
+		StepVerifier.create(fl)
+			.assertNext(a -> {
+				assertEquals(att1.getId(), a.getId());
+			})
+			.assertNext(a -> {
+				assertEquals(att2.getId(), a.getId());
 			})
 			.expectComplete()
 			.verify()
