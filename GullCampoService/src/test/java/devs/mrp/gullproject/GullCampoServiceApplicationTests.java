19a5bbb1c0c2a1ff2d315c02b0e599eeade6c359
miguel <48907268+zemua@users.noreply.github.com>
2021-07-03 19:22:13 +0200

    r

--- GullCampoService/src/test/java/devs/mrp/gullproject/GullCampoServiceApplicationTests.java
+++ GullCampoService/src/test/java/devs/mrp/gullproject/GullCampoServiceApplicationTests.java
@@ -2,8 +2,13 @@ package devs.mrp.gullproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import devs.mrp.gullproject.domains.Consulta;
+import devs.mrp.gullproject.domains.linea.LineaFactory;
 
 @SpringBootTest
+@Import({LineaFactory.class, Consulta.class})
 class GullCampoServiceApplicationTests {
 
 	@Test
