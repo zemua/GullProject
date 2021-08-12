ceb8bd28de89188dd9d7d00e81636ce743c131d4
miguel <48907268+zemua@users.noreply.github.com>
2021-08-12 19:26:04 +0200

    cors

--- GullCampoService/src/main/java/devs/mrp/gullproject/configuration/ResourceServerSecurityConfig.java
+++ GullCampoService/src/main/java/devs/mrp/gullproject/configuration/ResourceServerSecurityConfig.java
@@ -1,6 +1,7 @@
 package devs.mrp.gullproject.configuration;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,7 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
@@ -26,6 +27,15 @@ public class ResourceServerSecurityConfig {
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		return http
 				.csrf().disable()
+				.cors(c -> {
+					CorsConfigurationSource source = request -> {
+						CorsConfiguration config = new CorsConfiguration();
+						config.setAllowedOrigins(List.of("*"));
+						config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+						return config;
+					};
+					c.configurationSource(source);
+				})
 				.authorizeExchange()
 				.anyExchange()
 					.authenticated()
