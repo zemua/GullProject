0650decb411010080dca4d378446bbfa7d86e4e3
miguel <48907268+zemua@users.noreply.github.com>
2021-08-20 13:10:23 +0200

    adding configured keycloak

---
+++ keycloak-15.0.0/themes/keycloak/common/resources/web_modules/@patternfly/react-styles.js
@@ -0,0 +1,2 @@
+import"../common/_commonjsHelpers-02dfa760.js";export{S as StyleSheet,c as css,d as getClassName,b as getInsertedStyles,g as getModifier,a as isModifier,i as isValidStyleDeclaration,p as pickProperties}from"../common/StyleSheet-ba3bb0e5.js";
+//# sourceMappingURL=react-styles.js.map
