23ac289e2caed1ea2d6fd13cf7215c6a5cebe83d
zemua <48907268+zemua@users.noreply.github.com>
2021-08-31 17:48:52 +0200

    deleting unused service

--- GullValveService/src/main/java/devs/mrp/gullproject/repositorios/ParteRepo.java
+++
@@ -1,9 +0,0 @@
-package devs.mrp.gullproject.repositorios;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import devs.mrp.gullproject.domains.Parte;
-
-public interface ParteRepo extends ReactiveMongoRepository<Parte, String> {
-
-}
--- GullValveService/src/main/java/devs/mrp/gullproject/repositorios/PiezaRepo.java
+++
@@ -1,9 +0,0 @@
-package devs.mrp.gullproject.repositorios;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import devs.mrp.gullproject.domains.Pieza;
-
-public interface PiezaRepo extends ReactiveMongoRepository<Pieza, String> {
-
-}
